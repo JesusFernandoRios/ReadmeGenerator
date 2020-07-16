d947a4121795a6cb34154b9cf1cc0169cbe62994
Jrios <Jesusfernandorios93@gmail.com>
2020-07-15 18:30:44 -0700

    changed licence question to list, searched for licence badges

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 // array of questions for user
 const fs = require('fs');
 const inquirer = require("inquirer");
+const Choices = require('inquirer/lib/objects/choices');
 
 // const inquirer = require('inquirer')
 
-
-
+// licences for readme 
+// GNU [![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)](http://www.gnu.org/licenses/gpl-3.0)
+// MIT [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+// IBM PLV [![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)
 
 const questions = [
     {
@@ -29,20 +32,26 @@ const questions = [
           message: 'Please provide instruction on how to use your application.' 
       },
       {
-          type: 'input',
+          type: 'checkbox',
           name: 'license',
-          message: 'Would you like to licence your application? (ex. MIT, GPL , none)' 
+          message: 'Would you like to licence your application?(Select One)',
+          choices: ['MIT','GNU 3.0','IBM','None']
       },
       {
           type: 'input',
           name: 'contributors',
-          message: 'Would you like to list any contributors? if none type NA' 
+          message: 'Would you like to list any contributors?' 
       },
-    //   {
-    //       type: 'input',
-    //       name: 'bio',
-    //       message: 'Tell me something about yourself.' 
-    //   }
+      {
+          type: 'input',
+          name: 'test',
+          message: 'How can users Test your Application?' 
+      },
+      {
+        type: 'input',
+        name: 'contact',
+        message: 'How can users contact your for questions or concerns' 
+    }
 
 ];
 
@@ -56,7 +65,8 @@ function init() {
         console.log(answers)
 
 
-        const readMeContent = `# ${answers.title}
+        const readMeContent = `# ${answers.title}            
+## License ${answers.license}                                    
 
 ## Description
 ${answers.description}
@@ -64,23 +74,22 @@ ${answers.description}
 ## Table of Contents
 * [Questions](#Questions)
 * [Usage](#Usage)
-* [Installation](#Installation Guide)
+* [Installation](#Installation-Guide)
 * [Contributors](#Contributors)
+* [Contact-Information](#contact-Information)
 
-## Installation
+## Installation Guide
 ${answers.install}
         
 ## Usage
 ${answers.usage}
         
-## License
-${answers.license}
-        
 ## Contributors
-${answers.contributors}` 
+${answers.contributors}
 
-        
-
+## Contact Information
+${answers.contact}
+` 
 
     fs.writeFile('NewREADME.md', readMeContent, (err) =>{
         if(err){
